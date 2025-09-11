5132080ddf76d733a9eb8a70c19a6787ca823e5e
kimjiyoon <oneulrecord@naver.com>
2025-09-11 17:29:07 +0900

    0911 bday update

---
+++ 0911/1961_숫자 배열 회전.py
@@ -0,0 +1,9 @@
+T = int(input())
+
+for t in range(1, T+1):
+    n = int(input())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+
+
+    print(f'#{t}'/n)
+    print({90degree} {180degree} {270degree})
